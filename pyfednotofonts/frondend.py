a4b60b84441d763a21d893992acc3f2fbb5f8613
Akira TAGOH <akira@tagoh.org>
2023-01-19 15:21:37 +0900

    Initial commit

---
+++ pyfednotofonts/frondend.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2023 fedora-notofonts Authors
+# SPDX-License-Identifier: AGPL-3.0-or-later
+"""Module to package Noto Fonts from github."""
+
+import argparse
+import sys
+try:
+    from subcommand import FnSubcommand
+except ImportError:
+    from pyfednotofonts.subcommand import FnSubcommand
+from pyfontrpmspec.messages import Message as m
+from xdg import xdg_config_home
+
+
+def main():
+    """Endpoint function to package Noto Fonts from github."""
+    configfile = xdg_config_home() / '.fedora-notofonts'
+    if configfile.exists() is not True:
+        m([': ']).error(str(configfile)).message(
+            ('file not found. Please create API token from '
+             'https://github.com/settings/tokens '
+             'and put your token into a file as it is.')).out()
+        m([': ', ',']).message('Required scope is').info('public_repo').info(
+            'read:packages').info('read:project').out()
+        sys.exit(1)
+
+    token = configfile.read_text().rstrip()
+
+    parser = argparse.ArgumentParser(
+        description='Noto Fonts packaging helper',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    subcmds = FnSubcommand.load_subcommands()
+    subparsers = parser.add_subparsers()
+    for x in sorted(subcmds.keys()):
+        pp = subparsers.add_parser(subcmds[x].info.command,
+                                   help='see `{} -h`'.format(
+                                       subcmds[x].info.command))
+        subcmds[x].register(pp)
+        subcmds[x].set_token(token)
+        pp.set_defaults(handler=subcmds[x].handler)
+
+    args = parser.parse_args()
+    if hasattr(args, 'handler'):
+        args.handler(args)
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
---
+++ pyfednotofonts/subcommand.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2023 fedora-notofonts Authors
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""Sub-commands abstract module."""
+
+import glob
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+class FnSubcommand(object):
+    """Sub-command class to represent."""
+
+    _subcommands = {}
+
+    @classmethod
+    def load_subcommands(self):
+        """Load all the sub-commands available."""
+        cmds = []
+        subdir = Path(__file__).parent
+        files = [
+            Path(f).stem
+            for f in glob.glob(str(subdir / 'subcommands' / '*.py'))
+        ]
+        try:
+            mods = __import__('subcommands', globals(), locals(), files, 0)
+        except ImportError:
+            mods = __import__('pyfednotofonts.subcommands', globals(),
+                              locals(), files, 0)
+        for m in [getattr(mods, f) for f in files]:
+            cmds.extend(getattr(m, 'SUBCMD', []))
+        for c in cmds:
+            FnSubcommand._subcommands[c.info.command] = c
+
+        return FnSubcommand._subcommands
+
+    def __init__(self, cmd: str, helpstring: str):
+        """Initialize sub-command."""
+        self.info = FnSubcommandInfo(cmd, helpstring)
+
+    def register(self, parser: ArgumentParser) -> None:
+        """Abstract function to register a sub-command."""
+        self.do_register(parser)
+
+    def do_register(self, parser: ArgumentParser) -> None:
+        """Real function to register sub-command."""
+        pass
+
+    def set_token(self, token: str) -> None:
+        """Set a GitHub token."""
+        self.do_set_token(token)
+
+    def do_set_token(self, token: str) -> None:
+        """Real function to set a GitHub token."""
+        pass
+
+
+class FnSubcommandInfo(object):
+    """Sub-command Information class."""
+
+    def __init__(self, cmd: str, helpstring: str):
+        """Initialize SubcommandInfo class."""
+        self.command = cmd
+        self.help_string = helpstring
